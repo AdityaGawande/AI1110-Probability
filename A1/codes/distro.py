91fcbbd4ad0185dba8c1c7a7ba7a383d9258a756
Aditya Gawande <ee22btech11202@iith.ac.in>
2023-05-01 10:26:24 +0530

    Assignment 1 revision 5 - CDF function added

--- A1/codes/distro.py
+++ A1/codes/distro.py
@@ -1,15 +1,16 @@
 import math
 
-def BinomialProbability(n,p,x) :
-    pro = math.comb(n,x)*math.pow(1-p,n-x)*math.pow(p,x)
+def BinomialProbability(n,p,r) :
+    pro = math.comb(n,r)*math.pow(1-p,n-r)*math.pow(p,r)
     return pro
+
+def cdf(n,p,x) :
+    if x == -1:
+        return 0
+    sum = BinomialProbability(n,p,x)
+    return sum + cdf(n,p,x-1)
 
 n = 10
 p = 0.05
-i = 0
-while i < n+1:
-	print(f"P({i}) = {BinomialProbability(n,p,i)}")
-	i += 1
 
-
-print(f"\nP(0) = Probability of X <= 1 is P(0) + P(1) = {BinomialProbability(10, 0.05, 0)+BinomialProbability(10, 0.05, 1)}")
+print(f"\nP(0) = Probability of X <= 1 is CDF(1) = {cdf(n,p,1)}")
